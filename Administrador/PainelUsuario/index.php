b5467059061d324567177aa055eac48fbba9ddd5
dcs136 <dcs136@hotmail.com>
2021-01-14 16:18:53 -0300

    d

--- Administrador/PainelUsuario/index.php
+++ Administrador/PainelUsuario/index.php
@@ -19,7 +19,7 @@
 			</div>
 			<div class="img">
 <a href="https://app.powerbi.com/view?r=eyJrIjoiZjRkZDMyYzUtZWNlMi00ZGYwLWFmZmEtYTQ3MDVjODQyMjExIiwidCI6IjhkODQwNjdkLTlhZDctNDU3Mi05YjEwLTEzM2QzNjQ2MmFhYSIsImMiOjF9">
-	<img src="css/img.png" width="50" height="50">
+	<img src="css/img.png" width="100" height="100">
 </a>
 </div>
 		<!-- Inicio Modal -->
